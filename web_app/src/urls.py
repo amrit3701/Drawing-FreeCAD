c3fa40d12bf9c26c3615cac545099efffdd01812
Amritpal Singh <amrit3701@gmail.com>
2016-07-01 02:29:00 +0530

    imporve

--- web_app/src/urls.py
+++ web_app/src/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     #directs to index veiw
     url(r'^$', views.index, name='index'),
-    url(r'^/specs/$', views.specs, name='specs'),
-    url(r'^/specs/$', views.download, name='download')
+    url(r'^specs/$', views.specs, name='specs'),
+    url(r'^download/$', views.download, name='download')
 ]
--- web_app/src/views.py
+++ web_app/src/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os, threading, csv, tempfile
 #from django.core.servers.basehttp import FileWrapper
-from wsgiref.util import FileWrapper
+#from wsgiref.util import FileWrapper
 
 
 # Create your views here.
@@ -14,37 +14,42 @@ lists = {'stories':'','dep_of_foun':'','plinth_lev':'','cclear_height':'',
     'dep_slab':'','rep_span_len':'','rep_span_wid':'','col_type':'',
         'len_col':'','wid_col':'',  'radius_col':'','dep_beam':'',
             'wid_beam':''}
+lis = ['stories','dep_of_foun','plinth_lev','cclear_height','dep_slab','rep_span_len','rep_span_wid','col_type','len_col','wid_col','radius_col','dep_beam','wid_beam']
+
+#bb = []
 
 def specs(request):
     global lists
+    global lis
+    bb = list()
     for var in lists.keys():
-        lists[var] = request.POST.get(var)
-    print lists['wid_col']
-    print lists['rep_span_len']
+        request.session[var] = request.POST.get(var)
+        print("session  %s"  %request.session[var])
+    print lists
+#    print lists['rep_span_len']
 
+    for i in lis:
+        bb.append(request.POST.get(i))
+    print("list is : %s" %bb)
     f = open('some.csv', 'w')
     ww = csv.writer(f, delimiter=' ')
     a = []
-    for i in lists.keys():
-        print lists[i]
-        if lists[i] == None:
-            a.append(0)
-        else:
-            a.append(lists[i])
+    for i in bb:
+        a.append(i)
     ww.writerow(a)
     f.close()
     os.system('rm box.fcstd')
-    l = os.system('freecadcmd free_model.py')
-    print l
-    print request.POST
-    print len(request.POST)
+    l = os.system('freecadcmd macro_building_drawing.FCMacro')
+#    print l
+#    print request.POST
+#    print len(request.POST)
     return render(request, 'web_app/specs.html', {'lists': lists})
 
 
 
 def download(request):
-    command = "box.fcstd"
+    command = "project.fcstd"
     f = open(command)
     response = HttpResponse(f, content_type='application/fcstd')
-    response['Content-Disposition'] = 'attachment; filename="box.fcstd"'
+    response['Content-Disposition'] = 'attachment; filename="project.fcstd"'
     return response
