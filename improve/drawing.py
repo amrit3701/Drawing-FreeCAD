374163b1be732014cbf8f3a968fe960926e29fdd
Amritpal Singh <amrit3701@gmail.com>
2016-07-05 00:08:16 +0530

    update savepdf.py file

--- improve/drawing.py
+++ improve/drawing.py
@@ -1,19 +1,12 @@
-print 1
-import os 
-print 11                                                              
+import os
 import subprocess
-print 12                                                  
-#from PySide import QtGui                                               
-#import FreeCADGui                                                      
-import FreeCAD            
-print 13
-import Arch
-print 14
-import Draft
-print 15
-import Drawing       
+#from PySide import QtGui
+#import FreeCADGui
+import FreeCAD
+import Arch, Draft, Drawing
+from collections import OrderedDict
 
-print 2                                       
+print 2
 from building_func import *
 
 print 3
@@ -60,51 +53,51 @@ print 0
 # rotation: If it is 90 then the view on the drawing sheet is rotate
 # 90 degree.
 ########################################################################
-class obj_view_specs:                                                   
-    # Declaring a constructor                                           
+class obj_view_specs:
+    # Declaring a constructor
     def __init__(self, name, obj, x_dir, y_dir, z_dir, x_pos, y_pos, hid_lines,
-            scale_size, rotation, page):                                
-        self.name = name                                                
-        self.obj = obj                                                  
-        self.x_dir = x_dir                                              
-        self.y_dir = y_dir                                              
-        self.z_dir = z_dir                                              
-        self.x_pos = x_pos                                              
-        self.y_pos = y_pos                                              
-        self.hid_lines = hid_lines                                      
-        self.scale_size = scale_size                                    
-        self.rotation = rotation                                        
-        self.page = page                                                
-                                                                        
+            scale_size, rotation, page):
+        self.name = name
+        self.obj = obj
+        self.x_dir = x_dir
+        self.y_dir = y_dir
+        self.z_dir = z_dir
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.hid_lines = hid_lines
+        self.scale_size = scale_size
+        self.rotation = rotation
+        self.page = page
+
     ########################################################################
-    # The draw function projects the projections of the object on the   
+    # The draw function projects the projections of the object on the
     # drawing sheet. It takes the object of 'view_Specs' as argument, which
     # stores all the detail which are require to draw the drawing of the
-    # object on the drawing sheet.                                      
+    # object on the drawing sheet.
     ########################################################################
-    def draw_obj_view(view):                                            
-        App.ActiveDocument.addObject('Drawing::FeaturePage',view.page)  
-        page_ref = App.ActiveDocument.getObject(view.page)              
-# Choose the specific template                                          
+    def draw_obj_view(view):
+        App.ActiveDocument.addObject('Drawing::FeaturePage',view.page)
+        page_ref = App.ActiveDocument.getObject(view.page)
+# Choose the specific template
         page_ref.Template = App.getResourceDir()+'Mod/Drawing/Templates/A3_Landscape.svg'
-                                                                        
-                                                                        
-        # Add the object in the active document of specific name        
+
+
+        # Add the object in the active document of specific name
         App.ActiveDocument.addObject('Drawing::FeatureViewPart',view.name)
-        # view_ref stores the object having name view.name              
-        view_ref = App.ActiveDocument.getObject(view.name)              
-        # obj_ref stores the object having name view.obj                
-        obj_ref = App.ActiveDocument.getObject(view.obj)                
-        view_ref.Source = obj_ref                                       
-        view_ref.Direction = (view.x_dir, view.y_dir, view.z_dir)       
-        view_ref.X = view.x_pos                                         
-        view_ref.Y = view.y_pos                                         
-        view_ref.ShowHiddenLines = view.hid_lines                       
-        view_ref.Scale = view.scale_size                                
-        view_ref.Rotation = view.rotation                               
-        #page_ref = App.ActiveDocument.getObject(page_name)             
-        page_ref.addObject(view_ref)                                    
-        App.ActiveDocument.recompute() 
+        # view_ref stores the object having name view.name
+        view_ref = App.ActiveDocument.getObject(view.name)
+        # obj_ref stores the object having name view.obj
+        obj_ref = App.ActiveDocument.getObject(view.obj)
+        view_ref.Source = obj_ref
+        view_ref.Direction = (view.x_dir, view.y_dir, view.z_dir)
+        view_ref.X = view.x_pos
+        view_ref.Y = view.y_pos
+        view_ref.ShowHiddenLines = view.hid_lines
+        view_ref.Scale = view.scale_size
+        view_ref.Rotation = view.rotation
+        #page_ref = App.ActiveDocument.getObject(page_name)
+        page_ref.addObject(view_ref)
+        App.ActiveDocument.recompute()
 
 
 def center_of_storey(i):
@@ -135,11 +128,11 @@ class section_view_specs:
         self.page = page
 
     def draw_section_view(view):
-        App.ActiveDocument.addObject('Drawing::FeaturePage',view.page)  
-        page_ref = App.ActiveDocument.getObject(view.page)              
-        # Choose the specific template                                          
+        App.ActiveDocument.addObject('Drawing::FeaturePage',view.page)
+        page_ref = App.ActiveDocument.getObject(view.page)
+        # Choose the specific template
         page_ref.Template = App.getResourceDir()+'Mod/Drawing/Templates/A3_Landscape.svg'
- 
+
         obj_ref = App.ActiveDocument.getObject(view.obj)
         view_ref = Arch.makeSectionPlane([obj_ref])
         section_height = center_of_storey(view.storey)
@@ -155,18 +148,23 @@ class section_view_specs:
         draw_ref.Rotation = view.rotation
         App.ActiveDocument.recompute()
 
-view = obj_view_specs("view", "Compound", 0, 0, 1, 30, 100, False, 2, 0, "page")
-obj_view_specs.draw_obj_view(view)
+list_view = OrderedDict([('front', [1, 0, 0, 1.5]), ('side', [0, 1, 0, 1.5]), ('top', [0, 0, 1, 2])])
 
-viewIso = obj_view_specs("viewIso", "Compound", 1, 1, 1, 335, 60, True, 2, 120, "page2")
-obj_view_specs.draw_obj_view(viewIso)
+for i in list_view.keys():
+    arr = list_view[i]
+    page_name = "page_"+i
+    view = obj_view_specs(i, "Compound", arr[0], arr[1], arr[2], 150, 170, False, arr[3], -90, page_name)
+    obj_view_specs.draw_obj_view(view)
+
+#viewIso = obj_view_specs("viewIso", "Compound", 1, 1, 1, 335, 60, True, 2, 120, "page2")
+#obj_view_specs.draw_obj_view(viewIso)
 
 #import building_specs
 for i in range(0, stories):
 	a = "storey"+str(i)
-	sec_obj = section_view_specs("Compound", 10, 0, 0, 0, 0, 1, 0, 50, 200, 2, 0, i, a)
+	sec_obj = section_view_specs("Compound", 10, 0, 0, 0, 0, 1, 0, 150, 160, 3, 0, i, a)
 	section_view_specs.draw_section_view(sec_obj)
-	
+
 
 App.getDocument("project").saveAs("../web_app/project.fcstd")
 FreeCAD.closeDocument("project")
